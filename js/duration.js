c67c284b3af68ba97840f86fdc9e970eec4df664
tianqiqaq <tian_qi_awa@163.com>
2025-09-03 17:44:58 +0800

    Site updated: 2025-09-03 17:44:55

---
+++ js/duration.js
@@ -0,0 +1,34 @@
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/hint.css/2.4.1/hint.min.css">!(function() {
+  /** 计时起始时间，自行修改 **/
+  console.log("QaQ");
+  var start = new Date("2022/01/08 21:12:53");
+
+  function update() {
+    console.log("QAQ");
+    var now = new Date();
+    now.setTime(now.getTime()+250);
+    days = (now - start) / 1000 / 60 / 60 / 24;
+    dnum = Math.floor(days);
+    hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+    hnum = Math.floor(hours);
+    if(String(hnum).length === 1 ){
+      hnum = "0" + hnum;
+    }
+    minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+    mnum = Math.floor(minutes);
+    if(String(mnum).length === 1 ){
+      mnum = "0" + mnum;
+    }
+    seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+    snum = Math.round(seconds);
+    if(String(snum).length === 1 ){
+      snum = "0" + snum;
+    }
+    document.getElementById("timeDate").innerHTML = "本站陪伴您&nbsp"+dnum+"&nbsp天";
+    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    console.log("Qaq");
+  }
+
+  update();
+  setInterval(update, 1000);
+})();
